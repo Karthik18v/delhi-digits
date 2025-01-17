f751eb9b4d3ccf65fcacc75b9a8f8b3ea0b36097
Karthik18v <bittukarthik77@gmail.com>
2025-01-17 12:36:54 +0530

    completed

--- backend/routes/invoiceRouter.js
+++ backend/routes/invoiceRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const invoiceController = require("../controller/invoiceController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/", authMiddleware, invoiceController.getAllInvoices); // Get All Invoices
+router.get("/", invoiceController.getAllInvoices); // Get All Invoices
 router.get("/:id", authMiddleware, invoiceController.getInvoicesById); // Get Single Invoice By Id
 router.post("/", authMiddleware, invoiceController.addInvoices); // Post A Invoice
 router.put("/:id", authMiddleware, invoiceController.updateInvoiceById); // Update An Invoice By Id
